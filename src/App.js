1a18ce0c9bf73a39ab41d6fa57203b3e70388026
farah5198 <ghribifarah09@gmail.com>
2023-04-01 17:45:07 +0200

    upload csv file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 import CreateEtudiant from './components/crud_etudiant/create_etudiant/create_etudiant';
 import UpdateEtudiant from './components/crud_etudiant/update_etudiant/update_etudiant';
 import ReadEtudiant from './components/crud_etudiant/read_etudiant/read_etudiant';
-
+import UploadFileCSV from './components/crud_etudiant/uploadFileCSV/uploadFileCSV'
 function App() {
   return (
     <div className="App">
@@ -14,8 +14,8 @@ function App() {
   <Routes>
    <Route path="/create-etudiant" element ={<CreateEtudiant/>} />
    <Route path="/update-etudiant/:id" element ={<UpdateEtudiant/>} />
-
    <Route path="/readall-etudiant" element ={<ReadEtudiant/>} />
+   <Route path="/uploadfilecsv-etudiant" element ={<UploadFileCSV/>} />
 
   </Routes>
 </Router>
