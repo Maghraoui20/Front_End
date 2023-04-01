643e0c1baa27e1a3f998bb9d14a7c4b93a96dc3d
massarrabj <120268474+massarrabj@users.noreply.github.com>
2023-04-01 13:25:21 +0100

    update compte etudiant + update CV + theme dark/light recent ones

--- src/components/update_etudiant/update_etudiant.js
+++ src/components/update_etudiant/update_etudiant.js
@@ -1,206 +1,257 @@
 import {
-    TextField,
-    Typography,
-    Button,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Grid,
-    Paper
-  } from "@mui/material";
-  import { Container } from "@mui/system";
-  import React, { useEffect, useState } from "react";
-  import Box from "@mui/material/Box";
-  import * as api from "../../service/etudiant.js";
-  import { useNavigate , useParams} from "react-router-dom";
-  import moment from "moment";
-  import FileBase from 'react-file-base64';
+  TextField,
+  Typography,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  Paper,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import * as api from "../../service/etudiant.js";
+import { useNavigate , useParams} from "react-router-dom";
+import moment from "moment";
+import FileBase from 'react-file-base64';
 
 function UpdateEtudiant() {
-    
-    const params = useParams();
-    const [EtudiantData, setEtudiantData] = useState({
-        firstname: "",
-        lastname: "",
-        niveau: "",
-        classe: "",
-        Birth_date: "",
-        status: "",
-    });
-      const navigate = useNavigate();
-      const [niveau, setNiveau] = React.useState("");
-      const [status, setStatus] = React.useState("");
-      const [file, setFile] = useState();
-    
-      const handleChange = (e) => {
-          setEtudiantData({ ...EtudiantData, [e.target.name]: e.target.value });
-          console.log(EtudiantData);
-        };
-        
-        const handleChangeNiveau = (e) => {
-            setNiveau(e.target.value);
-            setEtudiantData({ ...EtudiantData, niveau: e.target.value });
-        };
-        const handleChangeStatus = (e) => {
-            setStatus(e.target.value);
-            setEtudiantData({ ...EtudiantData, status: e.target.value });
-        };
-        const handleSubmit = async (event) => {
-            event.preventDefault();
-            
-            try {
-                const updateEtudiant = await api.updateEtudiant(EtudiantData, params.id);
-                console.log(updateEtudiant);
-                navigate("/");
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        
-        
-        useEffect(() => {
-            async function fetchData() {
-              try{
-              const result = await api.getEtudiantbyid(params.id)
-              setEtudiantData(result)
-            } catch (e) {
-              console.log(e)
-            }}
-            fetchData()}, []) 
+  const params = useParams();
+  const [EtudiantData, setEtudiantData] = useState({
+    firstname: "",
+    lastname: "",
+    niveau: "",
+    classe: "",
+    Birth_date: "",
+    etat: "",
+    phone: "",
+    login: "",
+    email: "",
+  });
 
+  const navigate = useNavigate();
+  const [niveau, setNiveau] = React.useState("");
+  const [etat, setEtat] = React.useState("");
 
-        return (  <Container>
-    
-        <Paper elevation={3} className="paper"> 
+  const handleChange = (e) => {
+    setEtudiantData({ ...EtudiantData, [e.target.name]: e.target.value });
+  };
+
+  const handleChangeNiveau = (e) => {
+    setNiveau(e.target.value);
+    setEtudiantData({ ...EtudiantData, niveau: e.target.value });
+  };
+  const handleChangeEtat = (e) => {
+    setEtat(e.target.value);
+    setEtudiantData({ ...EtudiantData, etat: e.target.value });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const updateEtudiant = await api.updateEtudiant(EtudiantData, params.id);
+      console.log(updateEtudiant, "update");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await api.getEtudiantbyid(params.id);
+        setEtudiantData(result);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Paper elevation={3} className="paper">
         <Box
-sx={{
-  marginTop: 10,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-}}
->
-<form onSubmit={handleSubmit}>
-  <div     className="grid" >
-<Typography component="h1" variant="h5">
-  Modifier mon Profil {" "}
-</Typography>
-  <Grid
-    container
-    rowSpacing={1}
-    columnSpacing={{ xs: 1, sm: 2, md: 12 }}
-  >
-    <Grid item xs={6}>
-      <TextField
-        margin="normal"
-        required
-        value={EtudiantData.firstname}
-        fullWidth
-        id="firstname"
-        label="nom"
-        name="firstname"
-        autoFocus
-        onChange={handleChange}
-      />
-      <TextField
-        margin="normal"
-        required
-        value={EtudiantData.lastname}
-        fullWidth
-        id="lastname"
-        label="prénom"
-        name="lastname"
-        autoFocus
-        onChange={handleChange}
-      />
-
-      <FormControl fullWidth>
-        <InputLabel id="Niveau">Niveau</InputLabel>
-        <Select
-          labelId="Niveau"
-          id="Niveau"
-          value={EtudiantData.niveau}
-          label="Niveau"
-          name="niveau"
-          onChange={handleChangeNiveau}
+          sx={{
+            marginTop: 10,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          <MenuItem value={"licence"}>Licence</MenuItem>
-          <MenuItem value={"master"}>Master</MenuItem>
-          <MenuItem value={"cycle ingénieur"}>Cycle ingénieur</MenuItem>
-        </Select>
-      </FormControl>
-    </Grid>
-    <Grid item xs={6}>
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        value={EtudiantData.classe}
-        id="classe"
-        label="classe"
-        name="classe"
-        autoFocus
-        onChange={handleChange}
-      />
+          <form onSubmit={handleSubmit}>
+            <div className="grid">
+              <Typography component="h1" variant="h5">
+                Modifier Compte étudiant{" "}
+              </Typography>
+              <Box
+                sx={{
+                  marginTop: 5,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Grid
+                  container
+                  rowSpacing={1}
+                  columnSpacing={{ xs: 1, sm: 2, md: 12 }}
+                >
+                  <Grid item xs={6}>
+                    <TextField
+                      margin="normal"
+                      required
+                      value={EtudiantData.firstname}
+                      fullWidth
+                      id="firstname"
+                      label="nom"
+                      name="firstname"
+                      autoFocus
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      value={EtudiantData.lastname}
+                      fullWidth
+                      id="lastname"
+                      label="prénom"
+                      name="lastname"
+                      autoFocus
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="login"
+                      label="login"
+                      name="login"
+                      value={EtudiantData.login}
+                      autoFocus
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      label="email"
+                      name="email"
+                      value={EtudiantData.email}
+                      autoFocus
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="phone"
+                      label="phone"
+                      name="phone"
+                      value={EtudiantData.phone}
+                      autoFocus
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      value={EtudiantData.classe}
+                      id="classe"
+                      label="classe"
+                      name="classe"
+                      autoFocus
+                      onChange={handleChange}
+                    />
 
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        value={moment(EtudiantData.Birth_date).format('YYYY-MM-DD')}
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      value={moment(EtudiantData.Birth_date).format(
+                        "YYYY-MM-DD"
+                      )}
+                      id="Birth_date"
+                      label="date"
+                      name="Birth_date"
+                      autoFocus
+                      type="date"
+                      onChange={handleChange}
+                    />
 
-        id="Birth_date"
-        label="date"
-        name="Birth_date"
-        autoFocus
-        type="date"
-        onChange={handleChange}
-      />
-
-      <FormControl fullWidth>
-        <InputLabel id="staus">Status</InputLabel>
-        <Select
-          labelId="status"
-          id="status"
-          value={EtudiantData.status}
-          label="status"
-          onChange={handleChangeStatus}
-        >
-          <MenuItem value={"alumni"}>Alumni</MenuItem>
-          <MenuItem value={"actuel"}>Actuel</MenuItem>
-        </Select>
-      </FormControl>
-    </Grid>
-    <Grid item xs={6}>
-    <Typography> Importer votre CV </Typography>
-    <FileBase 
-    type="file" 
-    name="choisir un CV" 
-    multiple={false} 
-    onDone={({ base64 }) => setEtudiantData({ ...EtudiantData, Cv: base64 })} />
-    </Grid>
-    <Grid item xs={3}></Grid>
-    <Grid item xs={6}>
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        sx={{
-          mt: 3, mb: 2 ,
-          m: 4,
-          backgroundColor: "#00A36C",
-          ":hover": { backgroundColor: "#00A36C" },
-        }}      >
-        Modifier{" "}
-      </Button>
-    </Grid>
-  </Grid>
-</div>
-</form>
-</Box>
-</Paper>
-</Container> );
+                    <FormControl fullWidth sx={{ mt: 3 }}>
+                      <InputLabel id="Niveau">Niveau</InputLabel>
+                      <Select
+                        labelId="Niveau"
+                        id="Niveau"
+                        value={EtudiantData.niveau}
+                        label="Niveau"
+                        name="niveau"
+                        onChange={handleChangeNiveau}
+                      >
+                        <MenuItem value={"licence"}>Licence</MenuItem>
+                        <MenuItem value={"master"}>Master</MenuItem>
+                        <MenuItem value={"cycle ingénieur"}>
+                          Cycle ingénieur
+                        </MenuItem>
+                      </Select>
+                    </FormControl>
+                    <FormControl fullWidth sx={{ mt: 3 }}>
+                      <InputLabel id="staus">Etat</InputLabel>
+                      <Select
+                        labelId="etat"
+                        id="etat"
+                        value={EtudiantData.etat}
+                        label="etat"
+                        onChange={handleChangeEtat}
+                      >
+                        <MenuItem value={"alumni"}>Alumni</MenuItem>
+                        <MenuItem value={"actuel"}>Actuel</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography> Importer votre CV </Typography>
+                    <FileBase
+                      type="file"
+                      name="choisir un CV"
+                      multiple={false}
+                      onDone={({ base64 }) =>
+                        setEtudiantData({ ...EtudiantData, Cv: base64 })
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={3}></Grid>
+                  <Grid item xs={6}>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{
+                        mt: 3,
+                        mb: 2,
+                        m: 4,
+                        backgroundColor: "#00A36C",
+                        ":hover": { backgroundColor: "#00A36C" },
+                      }}
+                    >
+                      Modifier{" "}
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
+            </div>
+          </form>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
 
 export default UpdateEtudiant;
--- src/service/etudiant.js
+++ src/service/etudiant.js
@@ -1,11 +1,11 @@
 import Axios from "axios"
 
 export const updateEtudiant = async (EtudiantData, id) => {
-  const result = await Axios.put("http://localhost:5000/etudiants/updatebyid/"+id, EtudiantData)
+  const result = await Axios.put("http://localhost:5000/users/updatebyid/"+id, EtudiantData)
   return result.data
 }
 
 export const getEtudiantbyid = async (id) => {
-  const result = await Axios.get("http://localhost:5000/etudiants/getbyid/"+id)
+  const result = await Axios.get("http://localhost:5000/users/getbyid/"+id)
   return result.data
 }
