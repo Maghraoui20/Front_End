46d9cc207901774008142bd532a76602cb3f8d7f
dorra <boutitidorra@gmail.com>
2023-04-04 01:28:16 +0100

    forgot reset password

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import "./App.css";
 import { createContext, useState } from "react";
 import ReactSwitch from "react-switch";
 
-
 import UpdateEtudiantCV from "./components/update_etudiant/update_etudiant";
 
-import {
-  BrowserRouter as Router,Routes , Route
-} from 'react-router-dom'
-import Signin from './components/signin/signin.js';
-import CreateEnseignant from './components/crud_enseignant/create_enseignant/create_enseignant';
-import UpdateEnseignant from './components/crud_enseignant/update_enseignant/update_enseignant';
-import ReadEnseignant from './components/crud_enseignant/read_enseignant/read_enseignant';
-import CreateEtudiant from './components/crud_etudiant/create_etudiant/create_etudiant';
-import UpdateEtudiant from './components/crud_etudiant/update_etudiant/update_etudiant';
-import ReadEtudiant from './components/crud_etudiant/read_etudiant/read_etudiant';
-import UploadFileCSV from './components/crud_etudiant/uploadFileCSV/uploadFileCSV';
-import CreateEvenement from './components/crud_evenement/create_evenement/create_evenement';
-import UpdateEvenement from './components/crud_evenement/update_evenement/update_evenement';
-import ReadEvenement from './components/crud_evenement/read_evenement/read_evenement';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./components/signin/signin.js";
+import CreateEnseignant from "./components/crud_enseignant/create_enseignant/create_enseignant";
+import UpdateEnseignant from "./components/crud_enseignant/update_enseignant/update_enseignant";
+import ReadEnseignant from "./components/crud_enseignant/read_enseignant/read_enseignant";
+import CreateEtudiant from "./components/crud_etudiant/create_etudiant/create_etudiant";
+import UpdateEtudiant from "./components/crud_etudiant/update_etudiant/update_etudiant";
+import ReadEtudiant from "./components/crud_etudiant/read_etudiant/read_etudiant";
+import UploadFileCSV from "./components/crud_etudiant/uploadFileCSV/uploadFileCSV";
+import CreateEvenement from "./components/crud_evenement/create_evenement/create_evenement";
+import UpdateEvenement from "./components/crud_evenement/update_evenement/update_evenement";
+import ReadEvenement from "./components/crud_evenement/read_evenement/read_evenement";
+import ForgotPassword from "./components/forgot_password/index";
+import PasswordReset from "./components/PasswordReset/index";
 export const ThemeContext = createContext(null);
 function App() {
   const [theme, setTheme] = useState("dark");
@@ -31,21 +30,32 @@ function App() {
       <div className="App" id={theme}>
         <Router>
           <Routes>
-          <Route path="/signin" element ={ <Signin/>} />
+            <Route path="/signin" element={<Signin />} />
 
-<Route path="/create-enseignant" element ={<CreateEnseignant/>} />
-<Route path="/update-enseignant/:id" element ={<UpdateEnseignant/>} />
-<Route path="/readall-enseignant" element ={<ReadEnseignant/>} />
+            <Route path="/create-enseignant" element={<CreateEnseignant />} />
+            <Route
+              path="/update-enseignant/:id"
+              element={<UpdateEnseignant />}
+            />
+            <Route path="/readall-enseignant" element={<ReadEnseignant />} />
 
-   <Route path="/create-etudiant" element ={<CreateEtudiant/>} />
-   <Route path="/update-etudiant/:id" element ={<UpdateEtudiant/>} />
-   <Route path="/readall-etudiant" element ={<ReadEtudiant/>} />
-   <Route path="/update-etudiant-cv/:id" element={<UpdateEtudiantCV />} />
-    <Route path="/uploadfilecsv-etudiant" element ={<UploadFileCSV/>} />
+            <Route path="/create-etudiant" element={<CreateEtudiant />} />
+            <Route path="/update-etudiant/:id" element={<UpdateEtudiant />} />
+            <Route path="/readall-etudiant" element={<ReadEtudiant />} />
+            <Route
+              path="/update-etudiant-cv/:id"
+              element={<UpdateEtudiantCV />}
+            />
+            <Route path="/uploadfilecsv-etudiant" element={<UploadFileCSV />} />
 
-    <Route path="/create-evenement" element ={<CreateEvenement/>} />
-    <Route path="/update-evenement/:id" element ={<UpdateEvenement/>} />
-    <Route path="/readall-evenement" element ={<ReadEvenement/>} />
+            <Route path="/create-evenement" element={<CreateEvenement />} />
+            <Route path="/update-evenement/:id" element={<UpdateEvenement />} />
+            <Route path="/readall-evenement" element={<ReadEvenement />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route
+              path="/password-reset/:id/:token"
+              element={<PasswordReset />}
+            />
           </Routes>
         </Router>
         <div className="switch">
