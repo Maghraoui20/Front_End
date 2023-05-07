1bd76cc7095477174af28b564f581cf459325b2d
ameni maghraoui <maghraouiameni20@gmail.com>
2023-05-07 11:05:48 -0700

    changes

--- src/components/sidenavs/sidenav.js
+++ src/components/sidenavs/sidenav.js
@@ -16,49 +16,7 @@ function MySideNav() {
     localStorage.removeItem("token");
   };
 
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  const statutEtudiant = user?.etat;
-  let socket = null;
-const [count, setCount]= useState();
 
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-      
-        const result =  await api.getNotification(user._id);
-setCount(result.length);
-          if(!socket){
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            socket = io("http://localhost:5000");
-            socket.emit('join', user._id, (error) => {
-              if (error) {
-                alert(error);
-              }
-            });
-            socket.on('notification',  (notificationdata) => {
-            console.log("noo");
-              const result =   api.getNotification(user._id);
-              toast(notificationdata.title);
-
-             
-setCount(result.length);
-         
-             
-            });
-          }
-
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    fetchData();
-  }, []);
-const handleClick=async ()=>{
-navigate("/allnotification")
-const result = await  api.updateNotif(user._id);
-
-}
   return (
     <SideNav
       onSelect={(selected) => {
@@ -88,38 +46,7 @@ const result = await  api.updateNotif(user._id);
           </NavIcon>
           <NavText>Modifier CV</NavText>
         </NavItem>
-        {statutEtudiant == "actuel" ? (
-          <NavItem eventKey="insérer-stage-été">
-            <NavIcon>
-              <i className="fa fa-fw fa-hashtag" style={{ fontSize: "1em" }} />
-            </NavIcon>
-            <NavText>Stage été</NavText>
-          </NavItem>
-        ) : null}
-          {statutEtudiant == "actuel" ? (
-          <NavItem eventKey="mes-stage-été">
-            <NavIcon>
-              <i className="fa fa-fw fa-hashtag" style={{ fontSize: "1em" }} />
-            </NavIcon>
-            <NavText>Mes Stage d'été</NavText>
-          </NavItem>
-        ) : null}
-        {statutEtudiant == "actuel" ? (
-          <NavItem eventKey="insérer-stage-pfe">
-            <NavIcon>
-              <i className="fa fa-fw fa-hashtag" style={{ fontSize: "1em" }} />
-            </NavIcon>
-            <NavText>Stage pfe</NavText>
-          </NavItem>
-        ) : null}
-         {statutEtudiant == "actuel" ? (
-          <NavItem eventKey="mes-stage-pfe">
-            <NavIcon>
-              <i className="fa fa-fw fa-hashtag" style={{ fontSize: "1em" }} />
-            </NavIcon>
-            <NavText>Mes Stage de pfe</NavText>
-          </NavItem>
-        ) : null}
+     
         <NavItem eventKey="signin">
           <NavIcon>
             <i className="fa-regular fa-hashtag" style={{ fontSize: "1em" }} />
@@ -128,17 +55,7 @@ const result = await  api.updateNotif(user._id);
             <Button onClick={handleLogout}>Logout</Button>
           </NavText>
         </NavItem>
-        {statutEtudiant == "actuel" ? (
-
-      <div style={{display:"flex", flexDirection:"column"}}  
-           
-        >
-<IconButton >
- <Badge badgeContent={count} color="secondary" onClick={handleClick}>
-    <NotificationsActiveIcon />
-  </Badge>  
-</IconButton>
-      </div>):null}
+    
       </SideNav.Nav>
 
     </SideNav>
