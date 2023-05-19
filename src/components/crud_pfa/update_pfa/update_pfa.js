5d15959afc0ad4a0c0ee2fdcf0c168c78847d5d6
farah5198 <ghribifarah09@gmail.com>
2023-05-19 20:00:03 +0100

    chznges

--- src/components/crud_pfa/update_pfa/update_pfa.js
+++ src/components/crud_pfa/update_pfa/update_pfa.js
@@ -22,7 +22,7 @@ import MySideNav from "../../enseignant/sidenavEnseignant";
 function UpdatePfa() {
   const params = useParams();
 
-  const [technologies, setTechnologies] = React.useState([]);
+  const [technologies, setTechnologies] = React.useState([ ]);
   const [newTechnology, setNewTechnology] = React.useState("");
   const [PfaData, setPfaData] = useState({
     description: "",
@@ -32,9 +32,9 @@ function UpdatePfa() {
     technologies: [], // utiliser un tableau vide pour stocker les technologies sélectionnées
   });
 
-  const [TechnologieData, setTechnologieData] = useState({
+  const [TechnologieData, setTechnologieData] = useState([{
     title: "",
-  });
+  }]);
 
   const navigate = useNavigate();
 
@@ -47,7 +47,7 @@ function UpdatePfa() {
 
   const handleTechnologiesChange = (event, values) => {
     console.log("Selected Technologies:", values);
-    setPfaData({ ...PfaData, technologies: values.map((tech) => tech.title) }); // stocker les ObjectIds des technologies sélectionnées
+    setPfaData({ ...PfaData, TechnologieData:TechnologieData }); // stocker les ObjectIds des technologies sélectionnées
   };
 
   const handleNewTechnologyChange = (event) => {
@@ -77,10 +77,12 @@ function UpdatePfa() {
   };
 
   const options = technologies.map((tech) => ({
-    title: tech.title,
+    title: tech.title, // Change this to tech.title to use technology titles
     _id: tech._id,
   }));
 
+
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -99,8 +101,8 @@ function UpdatePfa() {
         const pfaResult = await api.getPfabyid(params.id);
         setPfaData(pfaResult);
 
-        const technologiesResult = await api.getAllTechnologies();
-        setTechnologies(technologiesResult.data);
+        const technologiesResult = await api.getTechnologiesByPfaId(params.id);
+        setTechnologieData(technologiesResult);
       } catch (e) {
         console.log(e);
       }
@@ -184,21 +186,38 @@ function UpdatePfa() {
                     onChange={handleChange}
                   />
 
-                  <Autocomplete
-                    multiple
-                    id="technologies-autocomplete"
-                    options={options}
-                    getOptionLabel={(option) => option.title}
-                    onChange={handleTechnologiesChange}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        variant="outlined"
-                        label="Technologies"
-                        placeholder="Sélectionnez des technologies"
-                      />
-                    )}
-                  />
+  <Autocomplete
+    multiple
+    id="technologies-autocomplete"
+    options={options}
+ 
+    value = {PfaData.technologies}
+    getOptionLabel={(option) => option.title}
+    onChange={handleTechnologiesChange}
+
+    renderTags={(value, getTagProps) =>
+      value.map((option, index) => (
+        <Chip
+          key={option._id}
+          variant="outlined"
+          label={option.title}
+          {...getTagProps({ index })}
+        />
+      ))
+    }
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        variant="outlined"
+        label="TechnologieData"
+        placeholder="Sélectionnez des technologies"
+      />
+    )}
+  />
+
+
+
+
                   <TextField
                     value={newTechnology}
                     onChange={handleNewTechnologyChange}
